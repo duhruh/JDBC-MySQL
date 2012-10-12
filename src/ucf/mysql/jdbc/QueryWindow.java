1efb970f13f858acb8ef842d6e2551233b88c9d6
David Rivera <David.R.Rivera193@gmail.com>
2012-10-12 11:58:55 -0400

    added close operation

--- src/ucf/mysql/jdbc/QueryWindow.java
+++ src/ucf/mysql/jdbc/QueryWindow.java
@@ -9,6 +9,8 @@ package ucf.mysql.jdbc;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.sql.SQLException;
 import java.util.Vector;
 
@@ -66,7 +68,7 @@ public class QueryWindow {
 	
 	private static SpringLayout mSpringLayout;
 	
-	private static DBEngine mDBEngine;
+	private static DBEngine mDBEngine = null;
 	private static DAO mDAO;
 	
 	public static void main(String[] args){
@@ -91,6 +93,56 @@ public class QueryWindow {
 		
 		mJFrame.add(mMainPanel);
 		
+		mJFrame.addWindowListener(new WindowListener(){
+
+			@Override
+			public void windowActivated(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+
+			}
+
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				if(mDBEngine != null){
+					try {
+						mDBEngine.CloseConnection();
+					} catch (SQLException e) {
+						System.out.println("Could not close connection, it's okay though application closing...");
+						mJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					}
+				}else{
+					mJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				}
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowIconified(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowOpened(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}});
 		
 		mJFrame.setVisible(true);
 	}
